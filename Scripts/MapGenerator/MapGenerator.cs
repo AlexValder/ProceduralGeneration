ed20cff4098ef4f0eb246920dfc26e26dfbec85f
AlexValder <anna.loban@yahoo.com>
2021-02-28 00:40:39 +0200

    Map Generation: -10% progress

--- Scripts/MapGenerator/MapGenerator.cs
+++ Scripts/MapGenerator/MapGenerator.cs
@@ -1,45 +1,91 @@
 using Godot;
 using System;
+using System.Text;
 
 public class MapGenerator : Node
 {
-    private bool _generated = false;
+    private Random _random;
+    private int _seed;
+    private bool _shouldEmptySeed = false;
 
-    public override void _Input(InputEvent @event)
+    private NodePath _lineEdit = "../ControlPanel/VBoxContainer/SeedInput";
+    private LineEdit _seedInput = null;
+
+    public override void _Ready()
     {
-        if (@event is InputEventKey e)
+        try
         {
-            if (e.Scancode == (uint)KeyList.M && e.IsPressed())
-            {
-                CreateTestMap();
-            }
+            _seedInput = (LineEdit)GetNode(_lineEdit);
+        }
+        catch (Exception ex)
+        {
+            GD.Print($"Exception: {ex.Message}");
         }
     }
 
     private void CreateTestMap()
     {
-        if (_generated) return;
-        _generated = true;
+        const int size = 20;
 
-        GD.Print("Started...");
-        var spatialScene = GetNode("/root/Spatial/");
+        var map = new int[size, size];
 
-        var st = new SurfaceTool();
-        
-        st.Begin(Mesh.PrimitiveType.TriangleFan);
+        GD.Print("Started.");
 
-        st.AddVertex(new Vector3(-1, 0, 1));
-        st.AddVertex(new Vector3(-1, 0, -1));
-        st.AddVertex(new Vector3(1, 0, -1));
+        GD.Print($"Seed retrieved: {_seed}");
 
-        var mi = new MeshInstance
+        var sb = new StringBuilder();
+
+        for (int i = 0; i < size; ++i)
         {
-            Name = "MAP",
-            Mesh = st.Commit()
-        };
+            for (int j = 0; j < size; ++j)
+            {
+                map[i, j] = _random.Next(0, 2);
+                if (map[i, j] == 0)
+                {
+                    sb.Append('o');
+                }
+                else
+                {
+                    sb.Append('x');
+                }
+            }
+            sb.Append('\n');
+        }
 
-        spatialScene.AddChild(mi);
-        GD.Print("Finished...");
-        GetNode("/root/").PrintTreePretty();
+        GD.Print("MAP:");
+        GD.Print(sb.ToString());
+
+        GD.Print("Finished.");
+    }
+
+    private void _on_GenerateMapButton_button_up()
+    {
+        try
+        {
+            if (_shouldEmptySeed)
+            {
+                _seedInput.Clear();
+            }
+
+            if (!_seedInput.Text.Empty())
+            {
+                _seed = _seedInput.Text.GetHashCode();
+                _random = new Random(_seed);
+                _shouldEmptySeed = false;
+            }
+            else
+            {
+                _seed = (int)DateTime.UtcNow.Ticks;
+                _random = new Random(_seed);
+                _seedInput.Text = _seed.ToString();
+                _shouldEmptySeed = true;
+            }
+        }
+        catch (Exception ex)
+        {
+            GD.Print($"Exception in signal: {ex.Message}\n{ex.StackTrace}");
+        }
+
+        CreateTestMap();
     }
 }
